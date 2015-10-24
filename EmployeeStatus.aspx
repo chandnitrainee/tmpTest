ec4c5a8a5439449940ba5094d2c4f60050bd90ce
chandnitrainee <chandni.trainee@yahoo.com>
2015-10-24 22:00:44 +0530

    aaaa

---
+++ EmployeeStatus.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Administrator/Administrator.master" AutoEventWireup="true" CodeFile="EmployeeStatus.aspx.cs" Inherits="EmployeeStatus" %>
+
+<%@ Register TagPrefix="telerik" Namespace="Telerik.Web.UI" Assembly="Telerik.Web.UI" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="Server">
+    <asp:ScriptManager ID="ScriptManager1" runat="server">
+    </asp:ScriptManager>
+    <div class="divInnerContentEmployeeStatus">
+        <h2>
+            <asp:Label runat="server" ID="lblCompanyName"></asp:Label></h2>
+        <h3>
+            <asp:Label runat="server" ID="lblHeaderDescription"></asp:Label></h3>
+        <asp:UpdatePanel runat="server" ID="updatePanel1">
+            <ContentTemplate>
+                <telerik:RadGrid AutoGenerateColumns="false" ID="employeeStatusGrid"
+                    AllowFilteringByColumn="True" AllowPaging="True" AllowSorting="True" runat="server"
+                    ShowGroupPanel="false" GroupingSettings-CaseSensitive="false"   OnNeedDataSource="employeeStatusGrid_NeedDataSource" OnItemCommand="employeeStatusGrid__ItemCommand" CellSpacing="0" GridLines="None"> 
+                    <PagerStyle Mode="NextPrevAndNumeric" />
+                    <MasterTableView TableLayout="Fixed">
+                        <Columns>
+                            <telerik:GridBoundColumn HeaderText="Employee Name" DataField="EmployeeName"
+                                SortExpression="EmployeeName" HeaderStyle-Width="160px" FilterControlWidth="160px"
+                                AutoPostBackOnFilter="true" CurrentFilterFunction="Contains" ShowFilterIcon="true" />
+                            <%--  <telerik:GridBoundColumn HeaderText="Last Entry" DataField="EntryDescription"
+                                SortExpression="LastEntry" HeaderStyle-Width="60px" FilterControlWidth="60px"
+                                AutoPostBackOnFilter="true" CurrentFilterFunction="Contains" ShowFilterIcon="true" />--%>
+                            <telerik:GridBoundColumn HeaderText="Total Hours Worked(HH:MM)" DataField="totalWorkHours" 
+                                SortExpression="totalWorkHours" HeaderStyle-Width="60px" FilterControlWidth="40px"
+                                AutoPostBackOnFilter="true" CurrentFilterFunction="Contains" ShowFilterIcon="true" />
+                             <telerik:GridBoundColumn HeaderText="Total Break Hours(HH:MM)" DataField="totalBreakHours"
+                                SortExpression="totalBreakHours" HeaderStyle-Width="60px" FilterControlWidth="40px"
+                                AutoPostBackOnFilter="true" CurrentFilterFunction="Contains" ShowFilterIcon="true" />
+                        </Columns>
+                    </MasterTableView>
+                </telerik:RadGrid>
+            </ContentTemplate>
+        </asp:UpdatePanel>
+    </div>
+</asp:Content>
+
---
+++ EmployeeStatus.aspx.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Telerik.Web.UI;
+
+public partial class EmployeeStatus : System.Web.UI.Page
+{
+    public string storeId;
+    wt_Store rStoreInfo = new wt_Store();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Session["StoreAccountInfo"] != null)
+        {
+             
+     
+            rStoreInfo = (wt_Store)Session["StoreAccountInfo"];
+            lblCompanyName.Text = rStoreInfo.store_name;
+            double timeOffset = TimeoffsetHelper.GetTimeoffsetValueFromTimeZone(rStoreInfo.store_TimeZone);
+            string dt = DateTime.UtcNow.AddHours(timeOffset).ToString("dddd,MM/dd/yyyy");
+     
+            lblHeaderDescription.Text = "Employee Current Satus for "+dt;
+        }
+        //if (!IsPostBack)
+        //{
+        //    bindGridView(rStoreInfo);
+        //}
+        
+    }
+
+
+    //public List<wt_StoreUserCurrentDayStatus> getStoreUesrCurrentDayStatusList(wt_Store rStoreInfo)
+    //{
+    //    List<wt_StoreUserCurrentDayStatus> wt_StoreUserCurrentDayStatusList = new List<wt_StoreUserCurrentDayStatus>();
+    //    double timeOffset = TimeoffsetHelper.GetTimeoffsetValueFromTimeZone(rStoreInfo.store_TimeZone);
+    //    string dt = DateTime.UtcNow.AddHours(timeOffset).ToString("MM/dd/yyyy");
+    //    string connectionSting = ConfigurationManager.ConnectionStrings["VrindiAttendance"].ConnectionString;
+    //    using (SqlConnection con = new SqlConnection(connectionSting))
+    //    {
+    //        using (SqlCommand cmd = new SqlCommand("Attendance_sp_get_StoreUserCurrentStatus"))
+    //        {
+    //            cmd.CommandType = CommandType.StoredProcedure;
+    //            cmd.Parameters.AddWithValue("@MasterAccountStoreID", rStoreInfo.storeId);
+    //            cmd.Parameters.AddWithValue("@date", dt);
+    //            cmd.Connection = con;
+    //            con.Open();
+
+    //            SqlDataReader idr = cmd.ExecuteReader();
+    //            while (idr.Read())
+    //            {
+    //                wt_StoreUserCurrentDayStatus wt_StoreUserCurrentDayStatus = new wt_StoreUserCurrentDayStatus();
+    //                if (idr["EmployeeName"] != DBNull.Value)
+    //                {
+    //                    wt_StoreUserCurrentDayStatus.employeeName = Convert.ToString(idr["EmployeeName"]);
+    //                }
+    //                //if (idr["CurrentStatus"] != DBNull.Value)
+    //                //{
+    //                //    wt_StoreUserCurrentDayStatus.currentStatus = Convert.ToString(idr["CurrentStatus"]);
+    //                //}
+    //                //if (idr["EntryDescription"] != DBNull.Value)
+    //                //{
+    //                //    wt_StoreUserCurrentDayStatus.EntryDescription = Convert.ToString(idr["EntryDescription"]);
+    //                //}
+    //                //if (idr["EntryDateTime"] != DBNull.Value)
+    //                //{
+    //                //    wt_StoreUserCurrentDayStatus.EntryDatetime = Convert.ToDateTime(idr["EntryDateTime"]);
+    //                //}
+    //                if (idr["totalHoursWork"] == DBNull.Value)
+    //                {
+    //                    wt_StoreUserCurrentDayStatus.totalWorkHours ="0";
+    //                }
+    //                else
+    //                {
+    //                    int minutes = 0, hours = 0, tot=0;
+    //                    tot = Convert.ToInt16(idr["totalHoursWork"]);
+    //                    minutes = tot % 60;
+    //                    hours = tot / 60;
+    //                    wt_StoreUserCurrentDayStatus.totalWorkHours =String.Format("{0}:{1}",hours,minutes);
+
+    //                }
+    //                if (idr["totalBreakHours"] == DBNull.Value)
+    //                {
+    //                    wt_StoreUserCurrentDayStatus.totalBreakHours = "0";
+    //                }
+    //                else
+    //                {
+    //                     int minutes = 0, hours = 0, tot = 0;
+    //                     tot = Convert.ToInt16(idr["totalBreakHours"]);
+    //                     minutes = tot % 60;
+    //                     hours = tot / 60;
+    //                     wt_StoreUserCurrentDayStatus.totalBreakHours = String.Format("{0}:{1}", hours, minutes);
+
+                     
+    //                }
+    //                wt_StoreUserCurrentDayStatusList.Add(wt_StoreUserCurrentDayStatus);
+    //            }
+    //            con.Close();
+    //        }
+    //    }
+    //    return wt_StoreUserCurrentDayStatusList;
+    //}
+
+    //public void bindGridView(wt_Store rStoreInfo)
+    //{
+    //    List<wt_StoreUserCurrentDayStatus> userCurrentDayStatusList = getStoreUesrCurrentDayStatusList(rStoreInfo);
+    //    employeeStatusGrid.DataSource = userCurrentDayStatusList;
+    //    //employeeStatusGrid.DataBind();
+    //    employeeStatusGrid.Rebind();
+    //}
+    protected void employeeStatusGrid_NeedDataSource(object source, Telerik.Web.UI.GridNeedDataSourceEventArgs e)
+    {
+        List<wt_StoreUserCurrentDayStatus> wt_StoreUserCurrentDayStatusList = new List<wt_StoreUserCurrentDayStatus>();
+        double timeOffset = TimeoffsetHelper.GetTimeoffsetValueFromTimeZone(rStoreInfo.store_TimeZone);
+        string dt = DateTime.UtcNow.AddHours(timeOffset).ToString("MM/dd/yyyy");
+        string connectionSting = ConfigurationManager.ConnectionStrings["VrindiAttendance"].ConnectionString;
+        using (SqlConnection con = new SqlConnection(connectionSting))
+        {
+            using (SqlCommand cmd = new SqlCommand("Attendance_sp_get_StoreUserCurrentStatus"))
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@MasterAccountStoreID", rStoreInfo.storeId);
+                cmd.Parameters.AddWithValue("@date", dt);
+                cmd.Connection = con;
+                con.Open();
+
+                SqlDataReader idr = cmd.ExecuteReader();
+                while (idr.Read())
+                {
+                    wt_StoreUserCurrentDayStatus wt_StoreUserCurrentDayStatus = new wt_StoreUserCurrentDayStatus();
+                    if (idr["EmployeeName"] != DBNull.Value)
+                    {
+                        wt_StoreUserCurrentDayStatus.employeeName = Convert.ToString(idr["EmployeeName"]);
+                    }
+                    //if (idr["LastEntry"] != DBNull.Value)
+                    //{
+                    //    wt_StoreUserCurrentDayStatus.EntryDescription = Convert.ToString(idr["LastEntry"]);
+                    //}
+                    //else
+                    //{
+                    //    wt_StoreUserCurrentDayStatus.EntryDescription = "No Entry Done";
+                    //}
+                    if (idr["totalHoursWork"] == DBNull.Value)
+                    {
+                        wt_StoreUserCurrentDayStatus.totalWorkHours = "0";
+                    }
+                    else
+                    {
+                        int minutes = 0, hours = 0, tot = 0;
+                        tot = Convert.ToInt16(idr["totalHoursWork"]);
+                        minutes = tot % 60;
+                        hours = tot / 60;
+                        wt_StoreUserCurrentDayStatus.totalWorkHours = String.Format("{0}:{1}", hours, minutes);
+
+                    }
+                    if (idr["totalBreakHours"] == DBNull.Value)
+                    {
+                        wt_StoreUserCurrentDayStatus.totalBreakHours = "0";
+                    }
+                    else
+                    {
+                        int minutes = 0, hours = 0, tot = 0;
+                        tot = Convert.ToInt16(idr["totalBreakHours"]);
+                        minutes = tot % 60;
+                        hours = tot / 60;
+                        wt_StoreUserCurrentDayStatus.totalBreakHours = String.Format("{0}:{1}", hours, minutes);
+
+
+                    }
+                    wt_StoreUserCurrentDayStatusList.Add(wt_StoreUserCurrentDayStatus);
+                }
+                con.Close();
+            }
+        }
+        employeeStatusGrid.DataSource = wt_StoreUserCurrentDayStatusList;
+    }
+
+
+  
+    protected void employeeStatusGrid__ItemCommand(object sender, GridCommandEventArgs e)
+    {
+        
+    }
+}
